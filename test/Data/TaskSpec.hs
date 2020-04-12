909a93038ec9fe31c189b1179f556c169e4fe7a6
Joseph Weston <joseph@weston.cloud>
2020-04-11 19:19:03 -0700

    Add failing tests for smart constructors

--- test/Data/TaskSpec.hs
+++ test/Data/TaskSpec.hs
@@ -1,8 +1,28 @@
 {-# options_ghc -Wno-orphans #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.TaskSpec (spec) where
 
+import Data.Text (Text)
+import qualified Data.Text as T
+import Test.QuickCheck
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck.Instances.Text ()
+
+import Data.Task
 
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "makePriority" $
+    prop "Only takes capital letters" $
+      \(p :: Char) -> False
+  describe "makeTag" $
+    prop "Only takes words" $
+      \(p :: Text) -> False
+  describe "makeTagType" $
+    prop "Only takes words" $
+      \(p :: Text) -> False
+  describe "makeDescription" $
+    prop "Only takes single lines" $
+      \(p :: Text) -> False
