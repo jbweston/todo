9045fb5623c97662c536483ce0d7d3b2932b44a5
Joseph Weston <joseph@weston.cloud>
2020-04-11 17:45:55 -0700

    Add smart constructors for datatypes

--- src/Data/Task.hs
+++ src/Data/Task.hs
@@ -1,24 +1,86 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module Data.Task
-  ()
+  ( Task
+  , Priority
+  , Tag
+  , TagType
+  , Description
+  , makePriority
+  , makeTag
+  , makeTagType
+  , makeDescription
+  , newTask
+  , getNewTask
+  )
 where
 
+import Data.Char (isSpace)
 import Data.Maybe
-import Data.Text
+import qualified Data.Text as T
+import Data.Text (Text)
 import Data.Time (Day)
-import Data.Word (Word)
+import Data.Time.LocalTime (getZonedTime, ZonedTime(..), LocalTime(..))
 
-newtype TaskPriority = TaskPriority Word
+import Control.Applicative
 
+-- Datatypes
+
+newtype Priority = Priority Char
 newtype Tag = Tag Text
-
+newtype TagType = TagType Text
+newtype Description = Description Text
 data Task = Task {
     completed :: Bool
-  , priority :: TaskPriority
-  , completion_date :: Maybe Day
-  , creation_date :: Maybe Day
-  , description :: Text
+  , priority :: Maybe Priority
+  , completionDate :: Maybe Day
+  , creationDate :: Maybe Day
+  , description :: Description
   , projects :: [Tag]
   , contexts :: [Tag]
-  , due_date :: Maybe Day
-  , tags :: [(Tag, Tag)]
+  , tags :: [Tag]
+  , dueDate :: Maybe Day
+  , extraTags :: [(TagType, Tag)]
 }
+
+-- Smart constructors
+
+makePriority :: Char -> Maybe Priority
+makePriority = Priority <$$> require (between 'A' 'Z')
+
+makeTag :: Text -> Maybe Tag
+makeTag = Tag <$$> require noSpaces
+
+makeTagType :: Text -> Maybe TagType
+makeTagType = TagType <$$> require noSpaces
+
+makeDescription :: Text -> Maybe Description
+makeDescription = Description <$$> require oneLine
+
+-- | Make a new Task with the provided creation date and description
+newTask :: Day -> Description -> Task
+newTask c d = Task False Nothing Nothing (Just c) d [] [] [] Nothing []
+
+-- | Make a new Task with the provided description and today as the creation date
+getNewTask :: Description -> IO Task
+getNewTask d = do
+    c <- localDay . zonedTimeToLocalTime <$> getZonedTime
+    pure $ newTask c d
+
+
+-- Utilities
+
+(.||.) = liftA2 (||)
+
+(<$$>) = fmap . fmap
+
+require :: (a -> Bool) -> a -> Maybe a
+require pred a = if pred a then Just a else Nothing
+
+between :: Ord a => a -> a -> a -> Bool
+between a b x = x >= a && x <= b
+
+noSpaces :: Text -> Bool
+noSpaces = not . T.any isSpace
+
+oneLine :: Text -> Bool
+oneLine = not . T.any (== '\n')
