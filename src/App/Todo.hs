c143608a8fd272cd78214045481a63282caf43e3
Joseph Weston <joseph@weston.cloud>
2020-04-26 12:18:11 -0700

    Avoid passing the UI thread the to filewatcher thread
    Instead the filewatcher thread just sleeps forever.

--- src/App/Todo.hs
+++ src/App/Todo.hs
@@ -8,7 +8,9 @@ where
 import Brick hiding (Context)
 import Brick.BChan
 
-import Control.Concurrent.Async
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (async, cancel, wait)
+import Control.Monad (forever)
 
 import Data.Functor
 import Data.Text.IO hiding (putStrLn)
@@ -47,8 +49,9 @@ main = do
   taskList <- parseOrDie fileName
   fileEvents <- newBChan 100
   uiThread <- async $ uiMain fileEvents (State fileName taskList)
-  _ <- async $ fileWatcher fileName fileEvents uiThread
+  fwThread <- async $ fileWatcher fileName fileEvents
   void $ wait uiThread
+  cancel fwThread
   where
     getFirstArg = Prelude.head <$> getArgs  -- obviously unsafe, but will do for now
     uiMain evChan state = do
@@ -61,10 +64,11 @@ main = do
         Left e -> die $ errorBundlePretty e
         Right tsks -> pure tsks
 
-fileWatcher :: FilePath -> BChan Ev -> Async a -> IO ()
-fileWatcher f chan end = FS.withManager $ \m ->
-  void $ FS.watchDir m dir fileChanged cb >> wait end
+fileWatcher :: FilePath -> BChan Ev -> IO ()
+fileWatcher f chan = FS.withManager $ \m ->
+  void $ FS.watchDir m dir fileChanged cb >> spin
   where
+    spin = forever $ threadDelay maxBound
     cb = const $ writeBChan chan TodoFileUpdated
     dir = takeDirectory f
     fileChanged (FS.Modified ef _ _) = (f == ef)
